e07597fc3bb5827718400deae69623fd6e637795
frimpongdapaah1 <84712231+frimpongdapaah1@users.noreply.github.com>
2025-07-25 10:49:50 +0000

    2

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,18 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <>
-            <h1>Recipe Sharing App</h1>
-            <AddRecipeForm />
-            <RecipeList />
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Recipe Sharing App</h1>
+              <AddRecipeForm />
+              <SearchBar />
+              <RecipeList />
+            </>
+          }
+        />
         <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
       </Routes>
     </Router>
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,11 +2,12 @@ import { useRecipeStore } from '../store/recipeStore';
 import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
 
   return (
     <div>
-      {recipes.map((recipe) => (
+      {filteredRecipes.length === 0 && <p>No recipes found.</p>}
+      {filteredRecipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>
             <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
