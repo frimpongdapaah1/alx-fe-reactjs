cbbec35b1aed829553c003377763b353735f9eaa
frimpongdapaah1 <84712231+frimpongdapaah1@users.noreply.github.com>
2025-08-10 22:42:40 +0000

    0

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,12 @@
+import Placeholder from './components/Placeholder';
+
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>GitHub User Search App</h1>
       <p>Setup complete. Ready for development.</p>
+      {}
+      <Placeholder />
     </div>
   );
 }
